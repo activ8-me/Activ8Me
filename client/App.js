006d6e563418c9a33773c4c532c43c96aef10887
Shandi Yuwono <shandi.yuwono@yahoo.com>
2019-07-27 16:58:42 +0700

    timepicker

--- client/App.js
+++ client/App.js
@@ -10,12 +10,14 @@ import React, {Fragment} from 'react';
 import { Container, Header, Content, Card, CardItem, Text, Body, CheckBox } from "native-base";
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { StyleSheet, View } from 'react-native'
-import AlarmList from './screens/AlarmList'
+import AlarmList from './screens/AlarmListShandi'
+import AlarmForm from './screens/AlarmFormShandi'
 
 const App = () => {
   return (
     <Fragment>
       <AlarmList />
+      {/* <AlarmForm /> */}
     </Fragment>
   );
 };

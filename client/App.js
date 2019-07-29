2e9b2d0ab6961ac0c624b17fb8b1f25e727d3e48
lyxcious <felix_laurensius@yahoo.com>
2019-07-29 10:33:31 +0700

    FCM setup

--- client/App.js
+++ client/App.js
@@ -1,14 +1,105 @@
-import React, {Fragment} from 'react';
+import React, {Component} from 'react';
 import Navigation from './navigation/AppNavigation'
 import {Provider} from 'react-redux'
 import store from './store'
+import AsyncStorage from '@react-native-community/async-storage';
+import { Alert } from 'react-native';
+import firebase from 'react-native-firebase';
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <Navigation />
-    </Provider>
+export default class App extends Component {
+
+  async componentDidMount() {
+    this.checkPermission();
+    this.createNotificationListeners(); //add this line
+  }
+  componentWillUnmount() {
+    this.notificationListener();
+    this.notificationOpenedListener();
+  }
+
+  //1
+async checkPermission() {
+  const enabled = await firebase.messaging().hasPermission();
+  if (enabled) {
+      this.getToken();
+  } else {
+      this.requestPermission();
+  }
+}
+
+  //3
+async getToken() {
+  let fcmToken = await AsyncStorage.getItem('fcmToken');
+  if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+          // user has a device token
+          await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+  }
+}
+
+  //2
+async requestPermission() {
+  try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+  } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+  }
+}
+
+
+async createNotificationListeners() {
+  /*
+  * Triggered when a particular notification has been received in foreground
+  * */
+  this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      this.showAlert(title, body);
+  });
+
+  /*
+  * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+  * */
+  this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      this.showAlert(title, body);
+  });
+
+  /*
+  * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+  * */
+  const notificationOpen = await firebase.notifications().getInitialNotification();
+  if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      this.showAlert(title, body);
+  }
+  /*
+  * Triggered for data only payload in foreground
+  * */
+  this.messageListener = firebase.messaging().onMessage((message) => {
+    console.log(JSON.stringify(message));
+  });
+}
+
+showAlert(title, body) {
+  Alert.alert(
+    title, body,
+    [
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ],
+    { cancelable: false },
   );
-};
+}
 
-export default App;
+  render() {
+    return (
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
+    );
+  }
+}
